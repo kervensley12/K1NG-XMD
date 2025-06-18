33e2301006e15e8482c350ebe1b93a7bcb2ae043
kervensley12 <kervensleysaintfleur74@gmail.com>
2025-06-18 13:50:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/ngnch5.jpeg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || "+",
+PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "K1NG-XMD",
 // add bot namw here for menu
